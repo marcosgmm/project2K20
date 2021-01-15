a650d01e035950d9f7f3e8a908d8639546da71b4
Alfredo <graziano.alfredo34@yahoo.com>
2021-01-15 23:49:40 +0100

    Modificato pagina istruttori e stile sito
    Mi mancano da allineare alcune pagine, commit provvisorio. Finita pagina home

--- php/istruttori.php
+++ php/istruttori.php
@@ -18,12 +18,13 @@ else {
 	/*if($chiuso == true) {echo "____database chiuso con successo____";} /*da togliere solo per test*/
 	$dlTennisti = "";
 	if ($tennisti != null){
-		$dlTennisti = "<dl>";
+		$dlTennisti = '<dl class="sport_list">';
 		foreach($tennisti as $tennista) {
 			$dlTennisti.='<dt>'.$tennista['nome'].' '.$tennista['cognome'].'</dt>';
 			$dlTennisti.='<dd>';
 			$dlTennisti.='<img class="inst_face" src="../images/'.$tennista['nomeimmagine'].'" alt=""/>';
 			$dlTennisti.='<p>'.$tennista['descrizione'].'</p>';
+			$dlTennisti.='<p class="tornaSu"><a href="#instructors">Torna su</a></p>';
 			$dlTennisti.='</dd>';
 		}
 		$dlTennisti.='</dl>';
@@ -34,12 +35,13 @@ else {
 
 	$dlCalciatori = "";
 	if ($calciatori != null){
-		$dlCalciatori = "<dl>";
+		$dlCalciatori = '<dl class="sport_list">';
 		foreach($calciatori as $calciatore) {
 			$dlCalciatori.='<dt>'.$calciatore['nome'].' '.$calciatore['cognome'].'</dt>';
 			$dlCalciatori.='<dd>';
 			$dlCalciatori.='<img class="inst_face" src="../images/'.$calciatore['nomeimmagine'].'" alt=""/>';
 			$dlCalciatori.='<p>'.$calciatore['descrizione'].'</p>';
+			$dlCalciatori.='<p class="tornaSu"><a href="#instructors">Torna su</a></p>';
 			$dlCalciatori.='</dd>';
 		}
 		$dlCalciatori.='</dl>';
@@ -50,13 +52,14 @@ else {
 
 	$dlPaddari = "";
 	if ($paddari != null){
-		$dlPaddari = "<dl>";
+		$dlPaddari = '<dl class="sport_list">';
 		foreach($paddari as $paddaro) {
 			$dlPaddari.='<dt>'.$paddaro['nome'].' '.$paddaro['cognome'].'</dt>';
 			$dlPaddari.='<dd>';
 			$dlPaddari.='<img class="inst_face" src="../images/'.$paddaro['nomeimmagine'].'" alt=""/>';
 			$dlPaddari.='<p>'.$paddaro['descrizione'].'</p>';
-			$dlCalciatori.='</dd>';
+			$dlPaddari.='<p class="tornaSu"><a href="#instructors">Torna su</a></p>';
+			$dlPaddari.='</dd>';
 		}
 		$dlPaddari.='</dl>';
 	}
@@ -66,12 +69,13 @@ else {
 
 	$dlNuotatori = "";
 	if ($nuotatori != null){
-		$dlNuotatori = "<dl>";
+		$dlNuotatori = '<dl class="sport_list">';
 		foreach($nuotatori as $nuotatore) {
 			$dlNuotatori.='<dt>'.$nuotatore['nome'].' '.$nuotatore['cognome'].'</dt>';
 			$dlNuotatori.='<dd>';
 			$dlNuotatori.='<img class="inst_face" src="../images/'.$nuotatore['nomeimmagine'].'" alt=""/>';
 			$dlNuotatori.='<p>'.$nuotatore['descrizione'].'</p>';
+			$dlNuotatori.='<p class="tornaSu"><a href="#instructors">Torna su</a></p>';
 			$dlNuotatori.='</dd>';
 		}
 		$dlNuotatori.='</dl>';
